75902ac804a73139b15490ab12ac4351f7ec7124
Sh1n000 <tlsworms10@gmail.com>
2025-04-29 20:46:49 +0900

    Update ui_main_window.py

--- scan_data_converter/python/io_manager/ui_main_window.py
+++ scan_data_converter/python/io_manager/ui_main_window.py
@@ -20,22 +20,36 @@ class IOManagerWindow(QMainWindow):
     def init_signals(self):
         self.ui.widget_dict["select_btn"].clicked.connect(self.select_scan_dir)
         self.ui.widget_dict["load_btn"].clicked.connect(self.load_metadata)
-        self.ui.widget_dict["project_combo_box"].currentIndexChanged.connect(
-            self.load_metadata
+        # Project Combo Box
+        self.ui.widget_dict["project_combo_box"].currentTextChanged.connect(
+            self.link_to_project
         )
 
     def select_scan_dir(self):
-        scan_dir_path = QFileDialog.getExistingDirectory(self, "Select Folder")
+        """
+        폴더만 선택가능
+        Select Scan Directory
+        Scan Data : EXR image sequence, MOV
+        """
+        # 폴더만 선택할 수 있게 제한
+        options = QFileDialog.Options()
+        options |= QFileDialog.ShowDirsOnly
+
+        scan_dir_path = QFileDialog.getExistingDirectory(
+            self,
+            "Select Folder",
+            self.ui.widget_dict["path_line_edit"].text(),  # 시작 폴더 (기본값)
+            options=options,
+        )
         if scan_dir_path:
             self.ui.widget_dict["path_line_edit"].setText(scan_dir_path)
 
     def get_project_list(self):
+        """show경로에서 Project Dir 리스트 반환"""
         path = "/show"
         project_list = []
-        for folder in os.listdir(path): 
-            if os.path.isdir(
-                os.path.join(path, folder)
-            ): 
+        for folder in os.listdir(path):
+            if os.path.isdir(os.path.join(path, folder)):
                 project_list.append(folder)
         return project_list
 
@@ -43,6 +57,17 @@ class IOManagerWindow(QMainWindow):
         project_list = self.get_project_list()
         self.ui.widget_dict["project_combo_box"].clear()
         self.ui.widget_dict["project_combo_box"].addItems(project_list)
+
+    def link_to_project_scan_dirs(self):
+        """User가 선택한 Project를 받아, scan 경로를 자동으로 연결"""
+        selected_project = self.ui.widget_dict["project_combo_box"].currentText()
+        if selected_project:
+            project_path = f"/show/{selected_project}"
+            scan_dirs_path = f"{project_path}/product/scan"
+            self.ui.widget_dict["path_line_edit"].setText(scan_dirs_path)
+            print(
+                f"[INFO] 프로젝트 선택: {selected_project}, 경로 설정: {scan_dirs_path}"
+            )
 
     def load_metadata(self):
         # 실제 메타데이터 로딩 구현 예정
